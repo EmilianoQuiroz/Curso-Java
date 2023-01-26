6fe89cc4cbc90e93b4a0e541233bfe47d1ec76f0
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-26 02:07:51 -0300

    Entrada de datos por consola +en Python

---
+++ 002-tipos_de_datos_en_python/02-entrada_de_datos.py
@@ -0,0 +1,11 @@
+#---- Entrada de datos por consola en Python ----#
+#Mediante la funcion input() podemos ingresar datos por consola en Python
+
+#Para esto creamos una variable y le asignamos como valor la variable input()
+#Esto hara que el dato que ingresemos por consola se asignara a la variable a la que le asignamos el input()
+
+resultado = input("Escribe un mensaje: ");#Dentro del input podemos imprimir el mensaje que querramos mostrar al usuario
+
+print(resultado);#Imprimimos la variable con el valor ingresado
+
+print("Fin del programa");
