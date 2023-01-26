4aa06cd3c7fed857aef7b0ddd156c35a50f8be5d
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-26 02:33:49 -0300

    Otros operadores en Python

---
+++ 003-operadores_en_python/operadores.py
@@ -0,0 +1,48 @@
+#---- Operadores aritmeticos en Python ----#
+"""
+Operadores Aritméticos.
+Operador	        Descripción	                                Uso
++           Realiza la suma de dos operandos                12 + 8 = 20
+-           Realiza la resta de dos operandos               12 - 6 = 6
+*	        Realiza Multiplicación entre los operandos	    12 * 3 = 36
+/	        Realiza División entre los operandos	        12 / 3 = 4
+%	        Realiza un módulo entre los operandos	        16 % 3 = 1
+**	        Realiza la potencia de los operandos	        12 ** 3 = 1728
+"""
+
+#--- Operadores de asignacion ---#
+"""
+OPERADOR	    DESCRIPCIÓN
+=	            a = 5. El valor 5 es asignado a la variable a
++=	            a += 5 es equivalente a a = a + 5
+-=	            a -= 5 es equivalente a a = a - 5
+*=	            a *= 3 es equivalente a a = a * 3
+/=	            a /= 3 es equivalente a a = a / 3
+%=	            a %= 3 es equivalente a a = a % 3
+**=	            a **= 3 es equivalente a a = a ** 3
+//=	            a //= 3 es equivalente a a = a // 3
+&=	            a &= 3 es equivalente a a = a & 3
+|=	            a |= 3 es equivalente a a = a | 3
+^=	            a ^= 3 es equivalente a a = a ^ 3
+>>=	            a >>= 3 es equivalente a a = a >> 3
+<<=	            a<<= 3 es equivalente a a = a << 3
+"""
+
+#--- Operadores de comparacion o relacionales ---#
+"""
+OPERADOR	DESCRIPCIÓN	USO
+>	        Devuelve True si el operador de la izquierda es mayor que el operador de la derecha	12 > 3 devuelve True
+<	        Devuelve True si el operador de la derecha es mayor que el operador de la izquierda	12 < 3 devuelve False
+==	        Devuelve True si ambos operandos son iguales	12 == 3 devuelve False
+>=	        Devuelve True si el operador de la izquierda es mayor o igual que el operador de la derecha	12 >= 3 devuelve True
+<=	        Devuelve True si el operador de la derecha es mayor o igual que el operador de la izquierda	12 <= 3 devuelve False
+!=	        Devuelve True si ambos operandos no son iguales
+"""
+
+#--- Operadores logicos en Python ---#
+"""
+OPERADOR	DESCRIPCIÓN	                                        USO
+and	        Devuelve True si ambos operandos son True	        a and b
+or	        Devuelve True si alguno de los operandos es True	a or b
+not	        Devuelve True si alguno de los operandos False	    not a
+"""
