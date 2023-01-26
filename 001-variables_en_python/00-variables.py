1174f0d684cad57f1a4731ecfa5af93cee2431d6
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-26 01:27:05 -0300

    Variables en Python

--- 001-variables_en_python/00-variables.py
+++ 001-variables_en_python/00-variables.py
@@ -4,6 +4,7 @@ Una variable nos va a permitir almacenar informacion de  manera temporal datos d
 """
 
 #Para declarar una variables debemos asignarle un nombre y mediante el signo "=" asignarle un valor
+#Estos valores que son los que se les asignan a las variables se llaman literales
 
 miVariable = "Cadena de texto";#En este caso asignamos una cadena de caracteres (string) a "miVariable"
 print(miVariable);#Imprimimos en pantalla el contenido de nuestra variable
@@ -12,4 +13,7 @@ print(miVariable);#Imprimimos en pantalla el contenido de nuestra variable
 miVariable = 1;#Podemos modificar el valor de nuestra variable con otro tipo de dato si ningun problema
 print(miVariable)#Imprimimos la variable con el nuevo valor
 
+#--- Ubicar espacios en memoria de una variable ---#
+#Para ubicar el espacio en  memoria que ocupa una variable podemos usar la funcion id()
 
+print(id(miVariable));#Esto nos dara la posicion en memoria de donde se encuentra nuestra variable
