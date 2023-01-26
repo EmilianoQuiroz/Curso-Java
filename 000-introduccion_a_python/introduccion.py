babcd0d9e0733ca642c830177ab99f6ea77eda3a
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-25 23:59:51 -0300

    Introduccion a Python

---
+++ 000-introduccion_a_python/introduccion.py
@@ -0,0 +1,11 @@
+#Los principales usos de Python son:
+#Desarrollo Web
+#Aplicaciones de Escritorio
+#Bases de datos
+#Redes
+#Cloud
+#Desarrollo de Videojuegos
+#Inteligencia Artificial
+#Ciencia de datos
+#Y mas...
+
