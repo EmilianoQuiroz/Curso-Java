da3bfe8eeb8e3cf2ada5ae40cb0445d8cef7dae4
EmilianoQuiroz <santiagoquiroz.dev@gmail.com>
2023-01-26 02:18:19 -0300

    Conversion de datos en Python

--- 002-tipos_de_datos_en_python/02-entrada_de_datos.py
+++ 002-tipos_de_datos_en_python/02-entrada_de_datos.py
@@ -1,5 +1,6 @@
 #---- Entrada de datos por consola en Python ----#
 #Mediante la funcion input() podemos ingresar datos por consola en Python
+#Por defecto esta funcion reecibe datos de tipo cadena de caracteres (string)
 
 #Para esto creamos una variable y le asignamos como valor la variable input()
 #Esto hara que el dato que ingresemos por consola se asignara a la variable a la que le asignamos el input()
@@ -9,3 +10,13 @@ resultado = input("Escribe un mensaje: ");#Dentro del input podemos imprimir el 
 print(resultado);#Imprimimos la variable con el valor ingresado
 
 print("Fin del programa");
+
+#--- Conversion de tipos de datos ---#
+#Para que un input reciba un tipo de dato que no sea el que tiene por defecto debemos meterlo dentro del tipo de dato que queremos que reciba por ejemplo : int(input(valor a recivir: ));
+
+numero1 = int(input("Digite un numero: "));
+numero2 = int(input("Digite otro numero: "));
+
+resultado = numero1 + numero2;
+
+print(resultado);
